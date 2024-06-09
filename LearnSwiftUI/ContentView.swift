1872569f18d4caec029bdfc9e7ff254eeb1e1765
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-08 21:34:00 -0400

    Initial commit

--- LearnSwiftUI/ContentView.swift
+++ LearnSwiftUI/ContentView.swift
@@ -10,12 +10,31 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack(spacing: 10) {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 100, height: 100)
+                Rectangle()
+                    .fill(.green)
+                    .frame(width: 100, height: 100)
+                
+                Rectangle()
+                    .fill(.blue)
+                    .frame(width: 100, height: 100)
+                    .rotationEffect(.degrees(30))
+            }.frame(maxWidth: .infinity, alignment: .leading)
+                .padding()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+                List(1...10, id: \.self) { index in
+                    Text("\(index)")
+                    
+                }
+            }
         }
-        .padding()
     }
 }
 
