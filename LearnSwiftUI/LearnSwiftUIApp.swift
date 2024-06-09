bd6f010a5bed606d0cf22175213166aae6bbb9b3
JECWorks <36215363+JECWorks@users.noreply.github.com>
2024-06-08 20:45:33 -0400

    Initial Commit

---
+++ LearnSwiftUI/LearnSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnSwiftUIApp.swift
+//  LearnSwiftUI
+//
+//  Created by Jason Cox on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LearnSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
